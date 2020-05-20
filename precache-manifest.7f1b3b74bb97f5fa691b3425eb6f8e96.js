acb69c11e84f36355213a15a3a2a7839221a7fdf
BhaktiShastri <bhaktishastri2021@gmail.com>
2020-05-20 14:43:12 -0400

    Updates

---
+++ precache-manifest.7f1b3b74bb97f5fa691b3425eb6f8e96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "012e56e54712c20aae83af5b22403a9b",
+    "url": "/Portfolio-with-Reactjs/index.html"
+  },
+  {
+    "revision": "15b7369bcee8ccaac6fc",
+    "url": "/Portfolio-with-Reactjs/static/css/main.026cfd3a.chunk.css"
+  },
+  {
+    "revision": "cc4d159a12a22f941c9e",
+    "url": "/Portfolio-with-Reactjs/static/js/2.d6ac3a40.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/Portfolio-with-Reactjs/static/js/2.d6ac3a40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b7369bcee8ccaac6fc",
+    "url": "/Portfolio-with-Reactjs/static/js/main.ac01cc60.chunk.js"
+  },
+  {
+    "revision": "b85c68c4673f05e915ce",
+    "url": "/Portfolio-with-Reactjs/static/js/runtime-main.c6302e54.js"
+  },
+  {
+    "revision": "8f5e624ef98a3f93f63bdd73adc30980",
+    "url": "/Portfolio-with-Reactjs/static/media/pic1.8f5e624e.jpg"
+  }
+]);
